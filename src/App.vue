2456b9895424dfad13e4b256f2e78a06dbc65de6
camCampus <camille.bochar@le-campus-numerique.fr>
2023-02-01 12:02:43 +0100

    Add component CitiesList

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import CitiesList from "@/components/CitiesList.vue";
+
+export default {
+  components: {
+    CitiesList
+  },
+  data() {
+  },
+  methods: {
+    onListClick() {
+      alert("connard")
+    }
+  }
+}
+</script>
+<template>
+<CitiesList></CitiesList>
+<button @click="onListClick" >CLICK</button>
+</template>
+
+<style scoped>
+
+
+</style>
